ce340c392e5b398da678b78e5d060047086b99e9
MirzoA <102673274+MirzoA@users.noreply.github.com>
2024-05-10 16:22:19 +0300

    Add files via upload

---
+++ Modul_2_homework6_dopolnitlnoe.py
@@ -0,0 +1,17 @@
+num = int(input('Введите число от 3 до 20: '))
+result = []
+if num >= 3 and num <= 20:
+    sum_ = 0
+    for i in range(1, num):
+        for j in range(i, num):
+            if i == j:
+                continue
+            sum_ = i + j
+            if num % sum_ == 0:
+                result.append(i)
+                result.append(j)
+
+    print(result)
+
+else:
+    print('Вы ошиблись')
